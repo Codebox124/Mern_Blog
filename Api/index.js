18546216085b37768896623bfa72aa49c3f77758
codebox124 <rajcodes733@gmail.com>
2023-08-18 05:29:41 -0700

    updated

--- Api/index.js
+++ Api/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
-const cors = require("cors")
+const cors = require("cors");
+const { default: mongoose } = require('mongoose');
+const User = require('./models/User')
 const app = express();
 
 app.use(cors());
 app.use(express.json())
 
-app.get('/register', (req, res)=>{
-    const {username, password} = req.body
-    res.json({requestData:{username, password}})
+ mongoose.connect("mongodb+srv://rajcodes733:Raji@olalekan1234@cluster0.chphjzz.mongodb.net/?retryWrites=true&w=majority")
+
+app.post('/register', async (req, res)=>{
+    const {username, password} = req.body;
+     const userDoc = await User.create({username,password})
+    res.json({userDoc})
 });
 app.listen("4000")
 
-//mongodb+srv://rajcodes733:<Raji@olalekan1234>@cluster0.chphjzz.mongodb.net/?retryWrites=true&w=majority
+//
